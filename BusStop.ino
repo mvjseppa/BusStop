6448745176502def43e100cb3762ca39ee765169
Mikko Seppälä <mikko.seppala@gofore.com>
2019-03-30 19:35:42 +0200

    add handler for i2c buttons on display shield

--- BusStop.ino
+++ BusStop.ino
@@ -1,6 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include <LOLIN_I2C_BUTTON.h>
 #include "BusClient.h"
 
 
@@ -13,7 +15,10 @@ char pwd[] = "";
 #define OLED_RESET 0  // GPIO0
 
 // driver is for 128 by 64 so we need some tweaking
-Adafruit_SSD1306 display(OLED_RESET); 
+Adafruit_SSD1306 display(OLED_RESET);
+I2C_BUTTON button(DEFAULT_I2C_BUTTON_ADDRESS); //I2C Address 0x31
+
+String keyString[] = {"None", "Press", "Long", "Double", "Hold"};
 
 void setup() {
   Serial.begin(115200);
@@ -22,49 +27,67 @@ void setup() {
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
   display.display();
-  
-  while (WiFi.status() != WL_CONNECTED)
-  {
+
+  while (WiFi.status() != WL_CONNECTED){
     wifiAnimation();
   }
-  Serial.println();
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
-
 }
 
-#define POST_INTERVAL 200 
-#define SCREEN_WIDTH 64
+#define TIMER_LOOPS 200
 
 BusStopClient busClient;
 
 void loop() {
+  int timer = TIMER_LOOPS;
 
-  static int scroll = 0;
-  static int scrollDelta = 1;
-  int timer = POST_INTERVAL;
-  
   busClient.updateStatus();
 
   String busTime = busClient.getTime();
   String busDetails = busClient.getDetails();
 
-  int textWidth = busDetails.length() * 6;
-  int maxScroll = max(textWidth - SCREEN_WIDTH, 0);
-
-  if(maxScroll == 0) scrollDelta = 0;
-  
   while(timer--){
-    if(scroll > maxScroll) scrollDelta = -1;
-    else if(scroll < 0) scrollDelta = 1;
-    
-    scroll += scrollDelta;
-    
-    showTextOnDisplay(busTime, busDetails, scroll);
+    processDisplay(busTime, busDetails);
+    processButton();
+
     delay(50);
   }
 }
 
+#define SCREEN_WIDTH 64
+
+void processDisplay(String& busTime, String& busDetails){
+  static int scroll = 0;
+  static int scrollDelta = 1;
+
+  int textWidth = busDetails.length() * 6;
+  int maxScroll = max(textWidth - SCREEN_WIDTH, 0);
+
+  if(maxScroll == 0) scrollDelta = 0;
+  else if(scroll > maxScroll) scrollDelta = -1;
+  else if(scroll < 0) scrollDelta = 1;
+  scroll += scrollDelta;
+
+  showTextOnDisplay(busTime, busDetails, scroll);
+}
+
+void processButton(){
+  if (button.get() == 0) // Button press
+  {
+    if (button.BUTTON_A)
+    {
+      Serial.print("A: ");
+      Serial.println(keyString[button.BUTTON_A]);
+    }
+
+    if (button.BUTTON_B)
+    {
+      Serial.print("B: ");
+      Serial.println(keyString[button.BUTTON_B]);
+    }
+  }
+}
 
 void showTextOnDisplay(String msg, String msg2, int scroll){
   display.clearDisplay();
@@ -79,19 +102,19 @@ void showTextOnDisplay(String msg, String msg2, int scroll){
 }
 
 // wifi reconnect, not tested
-void reconnect() {  
+void reconnect() {
   Serial.print("Reconnecting");
-  WiFi.mode(WIFI_STA);  
-  WiFi.begin(SSID, pwd);  
-  while (WiFi.status() != WL_CONNECTED) {  
-      delay(500);  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, pwd);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
       Serial.print(".");
-  }  
+  }
   Serial.println("Connected!");
 }
 
 void wifiAnimation(){
-  static const char pattern1[5][6] = {
+  static const char pattern[5][6] = {
     {"  .  "},
     {"  o  "},
     {"  O  "},
@@ -100,16 +123,16 @@ void wifiAnimation(){
   };
   static const char* pattern2 = ".oO .oO .oO .oO";
   const char* anim2 = pattern2 + strlen(pattern2);
-  
-  
+
+
   for(int i=0; i<5; i++){
 
     display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(35,12);
-    display.print(pattern1[i]);
+    display.print(pattern[i]);
     display.display();
     delay(10);
-  }  
+  }
 }
